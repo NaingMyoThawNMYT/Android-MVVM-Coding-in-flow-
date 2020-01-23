8e00148a41d927aec94f6861a2d9d24a7016ae54
NaingMyoThawNMYT <naingmyothaw.nmyt@gmail.com>
2020-01-23 07:53:02 +0630

    - Add NoteModel(Model and also Table).

--- app/src/main/java/com/example/androidmvvmcodinginflow/activity/MainActivity.java
+++ app/src/main/java/com/example/androidmvvmcodinginflow/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.androidmvvmcodinginflow;
+package com.example.androidmvvmcodinginflow.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.androidmvvmcodinginflow.R;
 
 public class MainActivity extends AppCompatActivity {
 
